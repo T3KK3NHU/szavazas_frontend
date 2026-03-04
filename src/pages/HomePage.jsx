810bf202e060dd38b24ab362ca864728b68d6003
diak <diak@mesterlevel.lo>
2026-03-04 11:41:49 +0100

    navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
+import Navbar from "../components/Navbar"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function HomePage(){
     return(
         <div>
+            <Navbar/>
             Home
         </div>
     )
